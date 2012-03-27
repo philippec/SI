8bd472c323a39c550cb2ce047446d5a32e956084
Philippe Casgrain <github@casgrain.com>
2012-03-26 20:23:23 -0400

    Renamed "MyCompanyName"

--- SITests/SITests.h
+++ SITests/SITests.h
@@ -3,7 +3,7 @@
 //  SITests
 //
 //  Created by Philippe Casgrain on 12-03-26.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Philippe Casgrain. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
